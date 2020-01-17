51cd1be9885d909e9f804f4b8ccf648e0ffc8501
1900498 <e1900498@shell.vamk.fi>
2020-01-17 14:48:27 +0200

    Added example

---
+++ datatypes.c
@@ -0,0 +1,21 @@
+/* Example using different data types in C
+Lauri Vuori*/
+
+#include <stdio.h>
+
+int main (void)
+{
+	int intergerVar = 100;
+	float floatingVar = 75.452;
+	double doubleVar = 5.23e+12;
+	char charVar = 'K';
+
+	_Bool boolVar = 0;
+
+	printf ("intergerVar = %i\n", intergerVar);
+	printf ("floatingVar = %f\n", floatingVar);
+	printf ("doubleVar = %e\n", doubleVar);
+	printf ("doubleVar = %g\n", doubleVar);
+	printf ("boolVar = %i\n", boolVar);
+return 0;
+}
