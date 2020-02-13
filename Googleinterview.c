cb2b217b48a47e8685a261a55ca98b942086d963
LauriVuori <laurivuori.lv@gmail.com>
2020-02-13 22:07:10 +0200

    Exercise from google interview

---
+++ Googleinterview.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+int arr[]={1,2,3,9};
+int a,b;
+printf("%d,",find_pair(arr,6,8,&a,&b));
+printf("%d %d",a,b);
+}
+
+int find_pair(int *arr, int len, int sum, int *a, int *b)
+{
+    int i;
+    int z;
+    int funsum = 0;
+    for(i = 0; i<= len; i++){
+        for(z = i+1; z<=len; z++){
+            funsum = arr[i]+arr[z];
+            if(funsum == sum){
+                *a=i;
+                *b=z;
+                return 1;
+            }
+            else
+            {
+                funsum=0;
+            }
+        }
+    }
+    return 0;
+}
