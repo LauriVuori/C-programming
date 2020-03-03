07f31c809c5d7259874d7c3624fb509279aa7f62
LauriVuori <laurivuori.lv@gmail.com>
2020-03-03 10:25:02 +0200

    Copy string with lib and without

--- CopyString2Style.c
+++ CopyString2Style.c
@@ -4,9 +4,12 @@
 int main(void){
 char s[25];
 char b[25];
+char c[25];
 int i;
 strcpy(s, "This is a string");
+strcpy(c, s);
 printf("s:%s\n", s);
+printf("c:%s\n", c);
 for(i = 0; s[i] != '\0'; i++){
     b[i] = s[i];
 }
