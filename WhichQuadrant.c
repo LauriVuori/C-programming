f4b1df3e072d2752244ed5c5c0e76f70bc839704
laurivuori <laurivuori.lv@gmail.com>
2020-03-12 21:07:53 +0200

    Determine which quadrant

--- WhichQuadrant.c
+++ WhichQuadrant.c
@@ -1,7 +1,5 @@
 /*Write a C function
-
 void which_quadrant(int x, int y);
-
 to accept a coordinate point in a XY coordinate system and determine in which quadrant the coordinate point lies
 
                    ^
