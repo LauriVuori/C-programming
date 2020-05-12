a035f0884d3c60cee9ea0810308794ce9c419342
Lauri Vuori <55624232+Nuubart@users.noreply.github.com>
2020-05-12 16:00:30 +0300

    Shoppin inventory exercise

---
+++ ShopInventory.c
@@ -0,0 +1,18 @@
+/*In shop there are 500 different products, that are identified by number 1-500.
+Make a program that can store amount of articles in a table. Program will 
+ask the product number and article amount and store them in one dimension table.
+Articles are inquired one by one, and after each article program will ask 
+if user will enter new product (yes/no-question).*/
+
+
+#include <stdio.h>
+
+
+int main(void){
+    int store[500], product, amount;
+
+    printf("Give product number?");
+    scanf("%d", product);
+
+    
+}
