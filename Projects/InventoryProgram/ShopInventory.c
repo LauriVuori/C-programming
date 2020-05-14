16d23038676319a1efdb81d97965d5b26154901e
Lauri Vuori <55624232+Nuubart@users.noreply.github.com>
2020-05-14 13:09:12 +0300

    Change amount and items, print changed

--- Projects/InventoryProgram/ShopInventory.c
+++ Projects/InventoryProgram/ShopInventory.c
@@ -6,7 +6,11 @@ if user will enter new product (yes/no-question).*/
 
 
 #include <stdio.h>
+
+
 #define ARRAY_SIZE 50
+
+
 struct product{
     int prod_amount;
     float prod_price;
@@ -14,45 +18,96 @@ struct product{
 
 void ask_prodamount(struct product info[ARRAY_SIZE]);
 void print_products(struct product info[ARRAY_SIZE]);
+void print_added_products(int item_num, int amount);
+char ask_command();
 void print_menu();
 
 int main(void){
-    struct product info[ARRAY_SIZE] = {0};
-    char command;
+struct product info[ARRAY_SIZE] = {0};
+char command;
 
+
+    print_menu();
     do{
-        print_menu();
-        scanf("%c", &command);
+
+        command = ask_command();
+
         switch (command)
         {
-        case '1':
+        case 'a':
+            print_menu();
+            break;
+
+        case 'b':
             ask_prodamount(info);
             break;
         
-        default:
+        case 'c':
+            print_products(info);
             break;
         }
     }while(command != 'n');    
 }
 
 void print_menu(){
-    printf("1: ask prods\n");
-    printf("n exit\n");
+    printf("A:print menu\n");
+    printf("B:Ask product amount:\n");
+    printf("C:Print products");
+    printf("N exit\n");
+}
+
+char ask_command(void){
+int A_to_a = 32;
+char return_command, temporal_array[ARRAY_SIZE];
+        
+    printf("Give command:\n");
+    fgets(temporal_array, ARRAY_SIZE, stdin);
+    sscanf(temporal_array, "%c", &return_command);
+
+    if (return_command >= 'A' && return_command <= 'Z' ){
+        return_command = return_command + A_to_a;
+    }
+    return return_command;
 }
 
 
 void ask_prodamount(struct product info[ARRAY_SIZE]){
-int amount = 0, item_num = 0;
-    printf("Give product name:\n");
-    scanf("%d", &item_num);
-    printf("Give product amount:\n");
-    scanf("%d", &amount);
+int amount = 0, item_num = 0, counter = 0;
+char temporal_array[ARRAY_SIZE], command = 'y';
+int changed_items[ARRAY_SIZE], changed_amounts[ARRAY_SIZE];
 
-    info[5].prod_amount = amount;
+    while(command == 'y'){
+        printf("Give product name/number:\n");
+            while(item_num <= 0 || item_num >= ARRAY_SIZE){
+                fgets(temporal_array, ARRAY_SIZE, stdin);
+                sscanf(temporal_array, "%d", &item_num);
+            }   
+
+        printf("Give product amount:\n");
+        fgets(temporal_array, ARRAY_SIZE, stdin);
+        sscanf(temporal_array, "%d", &amount);
+
+        info[item_num].prod_amount = amount;
+        changed_items[counter] = item_num;
+        changed_amounts[counter] = amount;
+        amount = 0;
+        item_num = 0;
+        counter++;
+        printf("Do you want to continue Y/N\n");
+        command = ask_command();
+    }
+
+    print_added_products(item_num, amount);
+}
+
+void print_added_products(int item_nums[ARRAY_SIZE], int item_amounts[ARRAY_SIZE]){
+    printf("You added following items:\n");
+    printf("Item number: %d\nAmount:%d\n", item_num, amount);
 }
 
 void print_products(struct product info[ARRAY_SIZE]){
 int i;
+
     for (i = 0; i < ARRAY_SIZE; i++){
         printf("%d: %d\n",i, info[i].prod_amount);
     }
