07390d5db2bb59d1c3c0373e51c187ac5ab04876
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2021-01-11 20:30:49 +0200

    random

---
+++ 28Rand45skiiers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(void){
+    int skiiers[45];
+
+    for (int i = 0; i < 45; i++){
+        skiiers[i] = 0;
+        printf("%d ", i);
+    }
+
+    srand((unsigned) time(NULL));
+
+    for (int i = 0; i < 45; i++){
+        skiiers[i] = 
+        printf("%d ", i);
+    }
+
+
+}
