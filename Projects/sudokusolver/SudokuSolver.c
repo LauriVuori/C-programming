536f7567dda080ead3b67d5278908c067b570a6e
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2021-01-26 19:55:52 +0200

    working row and colum check

--- Projects/sudokusolver/SudokuSolver.c
+++ Projects/sudokusolver/SudokuSolver.c
@@ -5,6 +5,9 @@
 void allocateMemorySudoku(int ** sudoku, int rowxheight);
 void solvesudoku(int sudoku[9][9]);
 void printSudoku(int * sudoku, int rowXcol);
+int checkRow(int * sudoku, int rowXcol);
+int checkColum(int * sudoku, int rowXcol);
+
 int main(void) {
     // int rowxheight = 9;
     // int **sudoku = malloc(sizeof(int[rowxheight])); // same, naming the type directly
@@ -28,9 +31,51 @@ int main(void) {
                         {3,0,0,0,1,0,0,5,0},
                         {6,0,4,0,0,0,0,0,2},
                         {0,0,5,9,0,0,0,0,0}};
+    int i = 0;
+    i = checkRow(&sudoku[1], 9);
+    printf("\n\n");
 
-    printSudoku(sudoku, 9);
+    i = checkColum(&sudoku[0][8], 9);
+    // printSudoku(sudoku, 9);
 }
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME:
+ DESCRIPTION: [0][x] tells which colum to check
+	Input: checkColum(&sudoku[0][x], 9);
+	Output:
+  Used global variables:
+  Used global constants:
+ REMARKS when using this function:
+*********************************************************************/
+int checkColum(int * sudoku, int rowXcol) {
+    int rows, *p;
+    for (rows = 0; rows < rowXcol; rows++) {
+        p = sudoku + rows * rowXcol;
+        printf("%d\n", *p);
+    }
+}
+
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME:
+ DESCRIPTION: [x] tells which row to check
+	Input: checkRow(&sudoku[x], 9);
+	Output:
+  Used global variables:
+  Used global constants:
+ REMARKS when using this function:
+*********************************************************************/
+int checkRow(int * sudoku, int rowXcol) {
+    int colums;
+    for (colums = 0; colums < rowXcol; colums++) {
+        printf("%d ", *(sudoku + colums));
+    }
+    return 0;
+}
+
 void printSudoku(int * sudoku, int rowXcol) {
     int *p, rows, colums;
     for(rows = 0; rows < rowXcol; rows++){
