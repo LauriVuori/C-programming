7ebcc2162b46ed4fbce5b24d2be72197ac844e97
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2021-01-28 15:50:14 +0200

    Working version, maybe add own sudokus

--- Projects/sudokusolver/SudokuSolver.c
+++ Projects/sudokusolver/SudokuSolver.c
@@ -2,18 +2,19 @@
 // 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define N 9
 
 
 int checkGrid(int * sudoku, int rowXcol, int checkNum);
-int isSafe(int grid[N][N], int row, int col, int num);
-int solveSuduko(int grid[N][N], int row, int col);
+int checkRules(int * sudoku, int row, int col, int num, int rowXcol);
+int solveSudoku(int * sudoku, int row, int colum, int rowXcolum);
 void allocateMemorySudoku(int ** sudoku, int rowxheight);
 void printSudoku(int * sudoku, int rowXcol);
 int checkRow(int * sudoku, int rowXcol, int checkNum);
 int checkColum(int * sudoku, int rowXcol, int checkNum);
+void suffleSudoku(int * sudoku, int rowXcol);
 
-void print(int arr[N][N]);
 int main(void) {
     // int rowxheight = 9;
     // int **sudoku = malloc(sizeof(int[rowxheight])); // same, naming the type directly
@@ -42,30 +43,44 @@ int main(void) {
                         {3,0,0,0,1,0,0,5,0},
                         {6,0,4,0,0,0,0,0,2},
                         {0,0,5,9,0,0,0,0,0}};
-    
-    // int i = 0;
-    // i = checkGrid(&sudoku[3][6], 9, 1);
-                        
-    if (solveSuduko(sudoku, 0, 0)==1)
-        print(sudoku);
-    else
+
+    if (solveSudoku(&sudoku[0][0], 0, 0, 9) == 1) {
+        printSudoku(&sudoku[0][0], 9);
+    }
+    else {
         printf("No solution exists");
- 
+    }
     return 0;
-    // int found = 5000;
-    // int checknum = 1;
-    // for (int i = 0; i < 9; i++) {
-    //     found = checkRow(&sudoku[i], 9, checknum);
-    //     found = checkColum(&sudoku[0][i], 9, checknum);
-
-    //     printf("\n\n");
-    //     printf("found == %d, checknum = %d \n", found, checknum);
-    //     printf("________________\n");
-    // }
-
-    // i = checkColum(&sudoku[0][1], 9);
-    // printSudoku(sudoku, 9);
 }
+
+
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME: suffleSudoku
+ DESCRIPTION: 
+	Input: 
+	Output:
+  Used global variables:
+  Used global constants:
+ REMARKS when using this function:
+*********************************************************************/
+void suffleSudoku(int * sudoku, int rowXcol) {
+    // rand if put number or not, rand num, check row to not same number, check colum not same number, go next spot,
+}
+
+
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME: checkGrid
+ DESCRIPTION: check 3x3 grid if there is number already
+	Input: sudoku, rows/colums and number to check
+	Output:
+  Used global variables:
+  Used global constants:
+ REMARKS when using this function:
+*********************************************************************/
 int checkGrid(int * sudoku, int rowXcol, int checkNum) {
     int *p, found = 0;
     for (int row = 0; row < 3; row++) {
@@ -79,12 +94,10 @@ int checkGrid(int * sudoku, int rowXcol, int checkNum) {
     return found;
 }
 
-
-
 /*********************************************************************
 	F U N C T I O N    D E S C R I P T I O N
 ---------------------------------------------------------------------
- NAME:
+ NAME: checkColum
  DESCRIPTION: [0][x] tells which colum to check
 	Input: checkColum(&sudoku[0][x], 9);
 	Output:
@@ -96,7 +109,6 @@ int checkColum(int * sudoku, int rowXcol, int checkNum) {
     int rows, *p, found = 0;
     for (rows = 0; rows < rowXcol; rows++) {
         p = sudoku + rows * rowXcol;
-        // printf("%d\n", *p);
         if (*p == checkNum) {
             found = 1;
         }
@@ -118,7 +130,6 @@ int checkColum(int * sudoku, int rowXcol, int checkNum) {
 int checkRow(int * sudoku, int rowXcol, int checkNum) {
     int colums, found = 0;
     for (colums = 0; colums < rowXcol; colums++) {
-        // printf("%d ", *(sudoku + colums));
         if (*(sudoku + colums) == checkNum) {
             found = 1;
         }
@@ -126,6 +137,17 @@ int checkRow(int * sudoku, int rowXcol, int checkNum) {
     return found;
 }
 
+/*********************************************************************
+	F U N C T I O N    D E S C R I P T I O N
+---------------------------------------------------------------------
+ NAME: Print sudoku
+ DESCRIPTION: 
+	Input: checkRow(&sudoku[x], 9);
+	Output:
+  Used global variables:
+  Used global constants:
+ REMARKS when using this function:
+*********************************************************************/
 void printSudoku(int * sudoku, int rowXcol) {
     int *p, rows, colums;
     for(rows = 0; rows < rowXcol; rows++){
@@ -137,92 +159,50 @@ void printSudoku(int * sudoku, int rowXcol) {
     }
 }
 
-
-int isSafe(int grid[N][N], int row, int col, int num) {
-
-    if (checkRow(&grid[row], 9, num) == 1) {
+int checkRules(int * sudoku, int row, int col, int num, int rowXcol) {
+    
+    if (checkRow((sudoku + row * rowXcol), rowXcol, num) == 1) {
         return 0;
     }
  
     // Check if we find the same num in the 
     // similar column , we return 0
 
-    if (checkColum(&grid[0][col], 9, num) == 1) {
+    if (checkColum(sudoku + col, 9, num) == 1) {
         return 0;
     }
     // Check if we find the same num in the 
     // particular 3*3 matrix, we return 0
     int startRow = row - row % 3, startCol = col - col % 3;
-    if (checkGrid(&grid[startRow][startCol],9 ,num) == 1) {
+    if (checkGrid(((sudoku + startRow * rowXcol) + startCol),9 ,num) == 1) {
         return 0;
     }
- 
+
     return 1;
 }
 
-int solveSuduko(int grid[N][N], int row, int col) {
-     
-    // Check if we have reached the 8th row 
-    // and 9th column (0
-    // indexed matrix) , we are 
-    // returning true to avoid
-    // further backtracking
-    if (row == N - 1 && col == N)
+int solveSudoku(int * sudoku, int row, int colum, int rowXcolum) {
+    if (row == rowXcolum - 1 && colum == rowXcolum) {
         return 1;
- 
-    //  Check if column value  becomes 9 ,
-    //  we move to next row and
-    //  column start from 0
-    if (col == N) 
-    {
-        row++;
-        col = 0;
     }
-   
-    // Check if the current position 
-    // of the grid already contains
-    // value >0, we iterate for next column
-    if (grid[row][col] > 0)
-        return solveSuduko(grid, row, col + 1);
- 
-    for (int num = 1; num <= N; num++) 
-    {
-         
-        // Check if it is safe to place 
-        // the num (1-9)  in the
-        // given row ,col  ->we move to next column
-        if (isSafe(grid, row, col, num)==1) 
-        {
-            /* assigning the num in the 
-               current (row,col)
-               position of the grid
-               and assuming our assined num 
-               in the position
-               is correct     */
-            grid[row][col] = num;
-           
-            //  Checking for next possibility with next
-            //  column
-            if (solveSuduko(grid, row, col + 1)==1)
+
+    if (colum == rowXcolum) {
+        row++;
+        colum = 0;
+    }
+    if (*((sudoku + row * rowXcolum)+ colum) > 0 ) {
+        return solveSudoku(sudoku, row, colum + 1, rowXcolum);
+    }
+
+    for (int num = 1; num <= rowXcolum; num++) {
+        if (checkRules(sudoku, row, colum, num, rowXcolum) == 1) {
+            *((sudoku + row * rowXcolum) + colum) = num;
+
+            if (solveSudoku(sudoku, row, colum + 1, rowXcolum) == 1) {
                 return 1;
+            }
         }
-       
-        // Removing the assigned num ,
-        // since our assumption
-        // was wrong , and we go for next 
-        // assumption with
-        // diff num value
-        grid[row][col] = 0;
+        *((sudoku + row * rowXcolum) + colum) = 0;
     }
     return 0;
-}
-
-void print(int arr[N][N])
-{
-     for (int i = 0; i < N; i++)
-      {
-         for (int j = 0; j < N; j++)
-            printf("%d ",arr[i][j]);
-         printf("\n");
-       }
 }
