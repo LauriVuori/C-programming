103d577df9d0070939cd1c49370df5f6c7058e88
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2021-02-05 17:17:19 +0200

    remove duplicates from array

--- removeDuplicatesIncArray.c
+++ removeDuplicatesIncArray.c
@@ -3,32 +3,34 @@ int remove_duplicate_array(int *a, int n);
 
 
 int main(void) {
-    int z[]={1,1,1,1,1,2,3,4,4,4,4};
-    int l=remove_duplicate_array(z,11);
-    printf("%d\n",l);
+int z[]={-111,1,-111,1,0,2,3,33,33,4,4,23,4,23};
+int l=remove_duplicate_array(z,14);
+printf("%d\n",l);
 
-    for(int i=0; i<l; i++)
-        printf("%d ",z[i]);
+for(int i=0; i<l; i++)
+    printf("%d ",z[i]);
 }
 
 int remove_duplicate_array(int *a, int n) {
-    int temp[50] = {0};
-    int counter = 0;
-    for (int i = 0; i < n; i++) {
-        // printf("%d", *a);
-        temp[*a]++;
-        a++;
-        counter++;
-    }
-    a -= counter;
-    counter = 0;
-    for (int i = 0; i < 50; i++) {
-        if (temp[i] > 0) {
-            // printf("<%d, %d>", temp[i], i);
-            *a = i;
-            counter++;
-            a++;
+    int i = 0, b = 0, counter = 0;
+
+    int temp[500];
+
+    for (i = 0; i < n; i++) {
+        for (b = 0; b < counter; b++) {
+            if (*(a+i) == temp[b]) {
+                b = counter;
+            }
         }
+        if (b == counter) {
+            temp[counter] = *(a+i);
+            counter++;
+        }
+        
+    }
+    for (int i = 0; i < counter; i++){
+        *a = temp[i];
+        a++;
     }
     return counter;
 }
