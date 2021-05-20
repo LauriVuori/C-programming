b78cad6c655abc8cb93c05b107a66840ef5587ea
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2021-05-20 19:19:51 +0300

    integer to char

---
+++ uint16tochars.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void UINT_TO_STRING(wint_t uInteger);
+
+int main(void) {
+    wint_t test = 23430;
+    UINT_TO_STRING(test);
+}
+void UINT_TO_STRING(wint_t uInteger) {
+    wint_t* pBuffer;
+    wint_t intBuffer [6];
+    pBuffer = &intBuffer[5];
+    *pBuffer = 0;
+    do {
+        *--pBuffer = (uInteger % 10) + '0';
+    } while( uInteger /= 10 );
+    while ( *pBuffer ) {
+        // Do something
+        *pBuffer++;
+    }
+}
