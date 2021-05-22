0f14d81026acaa10a1d2e7d77b6280f807976959
LauriVuori <Laurivuori.lv@gmail.com>
2021-05-22 17:54:20 +0300

    uint 16 to char

--- uint16tochars.c
+++ uint16tochars.c
@@ -8,13 +8,16 @@ int main(void) {
 void UINT_TO_STRING(wint_t uInteger) {
     wint_t* pBuffer;
     wint_t intBuffer [6];
+    int ii = 0;
     pBuffer = &intBuffer[5];
     *pBuffer = 0;
     do {
         *--pBuffer = (uInteger % 10) + '0';
     } while( uInteger /= 10 );
     while ( *pBuffer ) {
-        // Do something
+        intBuffer[ii] = *pBuffer;
         *pBuffer++;
+        ii++;
     }
+    printf("%s", intBuffer);
 }
