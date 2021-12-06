2bc9000dedb764028332eb3f99759728363a52c8
LauriVuori <Laurivuori.lv@gmail.com>
2021-12-06 15:28:24 +0200

    manual

--- modbus/modb_server/gtest_dir_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export GTEST_DIR=/mnt/e/googletest/googletest
-echo Current gtestdir is: $GTEST_DIR 
-export GTEST_DIR=/mnt/c/Users/lauri_svpuigm/OneDrive/Projektit/google_test_enviroment/googletest/googletest
